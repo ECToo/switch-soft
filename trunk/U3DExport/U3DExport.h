7591dd778ec00594b4071f6832bf60ee94be9bb7
roman.dzieciol <roman.dzieciol@e6f6c67e-47ec-11de-80ae-c1ff141340a0>
2009-05-23 23:35:09 +0000

    U3DExport imported

---
+++ trunk/U3DExport/U3DExport.h
@@ -0,0 +1,29 @@
+/**********************************************************************
+ *<
+	FILE: U3DExport.h
+
+	DESCRIPTION:	Includes for Plugins
+
+	CREATED BY:
+
+	HISTORY:
+
+ *>	Copyright (c) 2000, All Rights Reserved.
+ **********************************************************************/
+
+#ifndef __U3DEXPORT__H
+#define __U3DEXPORT__H
+
+#include "Max.h"
+#include "resource.h"
+#include "istdplug.h"
+#include "iparamb2.h"
+#include "iparamm2.h"
+
+
+
+extern TCHAR *GetString(int id);
+
+extern HINSTANCE hInstance;
+
+#endif // __U3DEXPORT__H
